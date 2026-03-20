3dd8da42a8797d90a43a7230347bc98ce94ea52a
Yuhan Zhao <956746759@qq.com>
2026-03-20 10:05:47 +0800

    修改astro.config.mjs配置base和site，npm run build生成文件，

--- pilot_license_theory/cn_pilot_theory_scr/astro.config.mjs
+++ pilot_license_theory/cn_pilot_theory_scr/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://aviationnote.com',
+	base: '/pilot_license_theory/cn_pilot_theory/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
